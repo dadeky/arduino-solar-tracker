53364c62af175d06cf4991518e1b1029f5993d62
dvorski <dadeky@gmail.com>
2018-06-19 13:00:47 +0200

    Small updates.

--- SolarTracker/Release/subdir.mk
+++ SolarTracker/Release/subdir.mk
@@ -3,9 +3,6 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-INO_SRCS += \
-..\SolarTracker.ino 
-
 CPP_SRCS += \
 ..\Actuator.cpp \
 ..\sloeber.ino.cpp 
@@ -13,9 +10,6 @@ CPP_SRCS += \
 LINK_OBJ += \
 .\Actuator.cpp.o \
 .\sloeber.ino.cpp.o 
-
-INO_DEPS += \
-.\SolarTracker.ino.d 
 
 CPP_DEPS += \
 .\Actuator.cpp.d \
@@ -26,21 +20,14 @@ CPP_DEPS += \
 Actuator.cpp.o: ..\Actuator.cpp
 	@echo 'Building file: $<'
 	@echo 'Starting C++ compile'
-	"D:\eclipse_oxygen\eclipse\/arduinoPlugin/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2/bin/avr-g++" -c -g -Os -Wall -Wextra -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10802 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-SolarTracker.o: ..\SolarTracker.ino
-	@echo 'Building file: $<'
-	@echo 'Starting C++ compile'
-	"D:\eclipse_oxygen\eclipse\/arduinoPlugin/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2/bin/avr-g++" -c -g -Os -Wall -Wextra -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10802 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"
+	"D:\eclipse_oxygen\eclipse\/arduinoPlugin/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2/bin/avr-g++" -c -g -Os -Wall -Wextra -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10802 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 sloeber.ino.cpp.o: ..\sloeber.ino.cpp
 	@echo 'Building file: $<'
 	@echo 'Starting C++ compile'
-	"D:\eclipse_oxygen\eclipse\/arduinoPlugin/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2/bin/avr-g++" -c -g -Os -Wall -Wextra -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10802 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"
+	"D:\eclipse_oxygen\eclipse\/arduinoPlugin/packages/arduino/tools/avr-gcc/4.9.2-atmel3.5.4-arduino2/bin/avr-g++" -c -g -Os -Wall -Wextra -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -flto -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=10802 -DARDUINO_AVR_NANO -DARDUINO_ARCH_AVR   -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\cores\arduino" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2" -I"D:\eclipse_oxygen\eclipse\arduinoPlugin\libraries\Servo\1.1.2\src" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -D__IN_ECLIPSE__=1 -x c++ "$<"  -o  "$@"
 	@echo 'Finished building: $<'
 	@echo ' '
 
